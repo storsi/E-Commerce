154f4a1275a1f71595e6f36d5b219345e7b1ccdb
storsi <spreamatto@gmail.com>
2023-01-03 19:04:31 +0100

    Filtro nella pagina "Prodotti"

--- js/prodotti.js
+++ js/prodotti.js
@@ -1,6 +1,11 @@
 const testoRicompra = document.getElementById("testoRicompra")
 const tuttiIProdotti = document.getElementById("prodotti")
 
+//check box
+const cb_informatica = document.getElementById("cb_informatica")
+const cb_alimentare = document.getElementById("cb_alimentare")
+const cb_cartoleria = document.getElementById("cb_cartoleria")
+
 const linkCSV = '/prodotti.csv'
 
 var data
@@ -22,5 +27,41 @@ function fillAllProducts() {
                 <p class = "costoProdotto">${data[i][1]}€</p>
             </div>
         `
+    }
+}
+
+function ridisponiProdotti() {
+    var tutto = true
+
+    tuttiIProdotti.innerHTML = ""
+
+    if(cb_informatica.checked) {
+        tutto = false
+        inserisciProdotti("Informatica")
+    } 
+    if(cb_alimentare.checked) {
+        tutto = false
+        inserisciProdotti("Alimentare")
+    } 
+    if(cb_cartoleria.checked) {
+        tutto = false
+        inserisciProdotti("Cartoleria")
+    } 
+
+    if(tutto) fillAllProducts()
+}
+
+function inserisciProdotti(categoria) {
+
+    for(i = 1; i < data.length; i++) {
+        if(data[i][3] == categoria) {
+            tuttiIProdotti.innerHTML += `
+                <div id = "prodotto">
+                    <img src = "${data[i][2]}" alt = "Immagine ${data[i][0]}" style="width: 80%">
+                    <p class = "titoloProdotto">${data[i][0]}</p>
+                    <p class = "costoProdotto">${data[i][1]}€</p>
+                </div>
+            `
+        }
     }
 }
